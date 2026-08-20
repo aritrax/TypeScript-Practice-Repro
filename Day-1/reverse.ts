64256c3ea19c19d9361c92f5dd2f228143dbb4c5
aritrax <aritrapaul2610@gmail.com>
2026-08-20 12:47:25 +0530

    ResversString

---
+++ Day-1/reverse.ts
@@ -0,0 +1,9 @@
+// Reverse a string 
+
+
+const word: string = "Hello" ;
+const word2: string[] = word.split("");
+const word3: string[] = word2.reverse();
+//console.log(word3)
+const result: string = word3.join("") ;
+console.log(result)
